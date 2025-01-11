943405d30474aa3b9ebbd56ee46a4863617d0d1b
Halo <13720369+Halo18@user.noreply.gitee.com>
2025-01-11 10:06:54 +0800

    开发分支

---
+++ src/main/java/com/example/aiquiz/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.example.aiquiz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+}
